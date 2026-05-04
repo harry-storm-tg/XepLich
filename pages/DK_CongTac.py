acc77cdc116fdc3fd46e3dc8738ab895d8947865
Beo_map <htthang.ths.dd23@gmail.com>
2026-05-04 23:12:42 +0700

    Thêm giao diện

--- pages/DK_CongTac.py
+++ pages/DK_CongTac.py
@@ -1,4 +1,90 @@
 import streamlit as st
-import pandas
+import pandas as pd
+import gspread
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import pathlib
+import base64
+from google.oauth2.service_account import Credentials
+import smtplib
+from email.mime.text import MIMEText
+import threading
 
-st.write("Đăng ký công tác")
+
+# ======================== UTILS ========================
+@st.cache_data(ttl=3600)
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+def load_css(file_path):
+    encoding = "utf-8"
+    try:
+        with open(file_path, "r", encoding=encoding) as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    except UnicodeDecodeError:
+        with open(file_path, "r", encoding="latin-1") as f:
+            st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+@st.cache_data(ttl=3600)
+def load_credentials():
+    creds_info = {
+    "type": st.secrets["google_service_account"]["type"],
+    "project_id": st.secrets["google_service_account"]["project_id"],
+    "private_key_id": st.secrets["google_service_account"]["private_key_id"],
+    "private_key": st.secrets["google_service_account"]["private_key"],
+    "client_email": st.secrets["google_service_account"]["client_email"],
+    "client_id": st.secrets["google_service_account"]["client_id"],
+    "auth_uri": st.secrets["google_service_account"]["auth_uri"],
+    "token_uri": st.secrets["google_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["google_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["google_service_account"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["google_service_account"]["universe_domain"],
+    }
+    # Dùng để kết nối Google APIs
+    credentials = Credentials.from_service_account_info(
+        creds_info,
+        scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+    )
+    return credentials
+
+@st.cache_data(ttl=3600)
+def load_data(x):
+    credentials = load_credentials()
+    gc = gspread.authorize(credentials)
+    sheet = gc.open(x).sheet1
+    data = sheet.get_all_values()
+    header = data[0]
+    values = data[1:]
+    data_final = pd.DataFrame(values, columns=header)
+    return data_final
+
+# ======================== UI COMPONENTS ========================
+
+
+
+# ======================== DIALOGS ========================
+
+
+
+# ======================== MAIN SECTION ========================
+
+css_path = pathlib.Path("asset/style.css")
+load_css(css_path)
+img = get_img_as_base64("pages/img/logo.png")
+st.markdown(f"""
+    <div class="fixed-header">
+        <div class="header-content">
+            <img src="data:image/png;base64,{img}" alt="logo">
+            <div class="header-text">
+                <h1>BỆNH VIỆN ĐA KHOA MỸ ĐỨC<span style="vertical-align: super; font-size: 0.6em;">&#174;</span></h1>
+            </div>
+        </div>
+        <div class="header-subtext">
+        <p>ĐĂNG KÝ LỊCH CÔNG TÁC</p>
+        </div>
+    </div>
+    <div class="header-underline"></div>
+    
+ """, unsafe_allow_html=True)
